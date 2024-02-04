8c147151b40e4e27ffed9ef290c26d9dd9155c75
Angie <liya0706s@gmail.com>
2024-02-04 23:00:34 +0800

    sql_all() math()內部函式 聚合函式

--- api/db.php
+++ api/db.php
@@ -33,7 +33,7 @@ class DB{
      }
 
     
-     //  用來簡化陣列參數的字串轉換
+     //  a2s() 用來簡化陣列參數的字串轉換
      /**
       * 此方法引僅供類別內部使用，外部無法呼叫
       * 帶入的參數必須為key-value型態的陣列
@@ -52,9 +52,84 @@ class DB{
         }
         // 回傳暫存的陣列
         return $tmp;
+    }
+        
+        // sql_all() 用來組合有特定條件，並且為多筆結果的sql語句
+        /**
+         * 此方法僅供類別內部使用，外部無法呼叫
+         * $sql 一個sql的字串，主要是where前的語法
+         * $array sql語句需要的欄位和值
+         * $other sql特殊語句
+         */
 
-        // 用來組合有特定條件
+        private function sql_all($sql,$array,$other){
+
+            // 如果有設定資料表且不為空
+            if(isset($this->table) && !empty($this->table)){
+
+                // 如果參數為陣列
+                if(is_array($array)){
+
+                    // 如果陣列不為空
+                    if(!empty($array)){
+                        $tmp=$this->a2s($array);
+                        $sql .= " where " .join(" && ", $tmp);
+                    }
+                }else{
+                    $sql .= " $array";
+                }
+
+                $sql .= $other;
+
+                // 回傳sql字串
+                return $sql;
+            }
+        }
+
+        // math() 內部函式，用來做為聚合函式的sql語法轉換
+        protected function math($math,$col,$array='',$other=''){
+            $sql="select $math($col) from $this->table ";
+            $sql=$this->sql_all($sql,$array,$other);
+
+            // 因為這類方法大多是只會回傳一個值，所以使用fetchColumn()的方法來回傳
+            return $this->pdo->query($sql)->fetchColumn();
+        }
+
+
+        // sum()/max()/min()... 使用聚合函式來計算某個欄位的結果
+        /**
+         * 直接呼叫內部的方法math()，帶入需要的參數即可
+         * 這樣設計的目的是為了讓外部呼叫時方法名稱比較直覺，
+         * 同時也減少需要帶入的參數
+         */
+        function sum($col,$where='',$other=''){
+            return $this->math('sum',$col,$where,$other);
+        }
+
+        function max($col, $where='',$other=''){
+            return $this->math('max',$col,$where,$other);
+        }
+        function min($col,$where='',$other=''){
+            return $this->math('min',$col,$where,$other);
+        }
+
+        // count()... 使用聚合函式來計算查詢到的資料筆數
+        /**
+         * 執行 SQL 語句並回傳結果
+         * 
+         * param string $sql SQL語句
+         * param string $where WHERE條件
+         * param string $other 其他條件
+         * return int 回傳結果
+         */
+        function count($where='',$other=''){
+            // 建立查詢資料筆數的SQL語句
+            $sql="select count(*) from `$this->table` ";
+            // 拼接 WHERE 條件和其他條件
+            $sql=$this->sql_all($sql,$where,$other);
+            // 執行 SQL語句並回傳結果
+            return $this->pdo->query($sql)->fetchColumn();
+        }
       }
-}
 
 ?>
