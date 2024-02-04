49070484a2e6d0f0cb80be3494f8638febaf3a13
Angie <liya0706s@gmail.com>
2024-02-04 23:22:41 +0800

    all() find()

--- api/db.php
+++ api/db.php
@@ -130,6 +130,44 @@ class DB{
             // 執行 SQL語句並回傳結果
             return $this->pdo->query($sql)->fetchColumn();
         }
+
+        // 撰寫外部空開方法
+        // $table->all() 查詢符合條件的全部資料
+        /**
+         * 此方法主要用來取得符合條件的所有資料
+         */
+        function all($where='',$other=''){
+            // 建立一個基礎語法字串
+        $sql="select * from $this->table ";
+
+            // 將語法字串及參數帶入到類別內部的sql_all()方法中，結果會得到一個完整的SQL句子
+        $sql=$this->sql_all($sql,$where,$other);
+
+        // 將sql句子帶進pdo的query方法中，並以fetchAll的方式回傳所有的結果
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+
+        // $able->find($id) 查詢符合條件的單筆資料
+        function find($id){
+            // 建立一個基礎語法字串
+            $sql="select * from $this->table ";
+
+            // 如果 $id 是陣列
+            if(is_array($id)){
+                // 執行內部方法a2s
+                $tmp=$this->a2s($id);
+                // 拼接sql語句
+                $sql .= " where " .join(" && ", $tmp);
+            }
+            // 如果 $id 是數字
+            else if(is_numeric($id)){
+                // 拼接sql語句
+                $sql .= " where `id`='$id'";
+            }
+            // 將sql句子帶進pdo的query方法中，並以fetch的方式回傳一筆資料結果
+            return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        }
       }
 
 ?>
